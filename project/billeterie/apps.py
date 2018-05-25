a018a96c51ebe1d5afa536f765bdf6775bc549e5
Raphaël QUEIROZ <queiro_r@epita.fr>
2018-05-25 15:41:12 +0200

    up

---
+++ project/billeterie/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BilleterieConfig(AppConfig):
+    name = 'billeterie'
---
+++ project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dayblox/Projects/YaKaChoisir/project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
